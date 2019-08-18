dde45274441ce787ee97e9d64f5461ddad007204
MovingToWeb <soleil.fils.88@gmail.com>
2019-08-18 09:36:29 +0530

    [update] automated travis deployment

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,6 @@ git config --global user.name "Travis CI"
 hugo
 
 # deploy
-cd public
-git init
 git add .
 git commit -m "Deploy to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
