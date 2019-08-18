cfba9b75daf6a73ea62b0aee49cb86733f75ad14
MovingToWeb <soleil.fils.88@gmail.com>
2019-08-18 09:48:27 +0530

    [update] automated travis deployment

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,16 @@
 set -o errexit
 
 echo "Deployment in progress..."
-rm -rf public
-echo "Removed `public` directory"
-mkdir -p public
-echo "Create `public` directory"
 
 # config
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 echo "Added Github username and email address"
 
-# build
-hugo
-echo "Hugo build successful"
-
 # deploy
 git add .
 git commit -m "Deploy to Github Pages"
 echo "Commit successful"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/techprimers/techprimers.github.io.git" master:master > /dev/null 2>&1
 echo "Push successful"
 echo "Deployment successful"
