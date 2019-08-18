65bee38adee0f3c564f745d30ddcee6ee79f0a4d
MovingToWeb <soleil.fils.88@gmail.com>
2019-08-18 09:25:35 +0530

    [update] automated travis deployment

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# See reference: https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+rm -rf public
+mkdir public
+
+# config
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
+# build
+hugo
+
+# deploy
+cd public
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
